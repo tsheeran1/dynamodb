33f4cc8f1b5d4e8522175f012ada785b39998e65
tsheeran1 <tom@beast.lan>
2019-05-03 19:05:50 -0400

    7pm commit

---
+++ httpget.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func httpget() {
+	fmt.Println("httpget")
+}
--- main.go
+++ main.go
@@ -1,47 +1,19 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
-)
 
-type Record struct {
-	Userid string
-	Age    int `json:"age"`
-	Height int `json:"height"`
-	Income int `json:"income"`
-}
+	"os"
+)
 
 func main() {
 
-	data, err := ioutil.ReadFile("input.json")
-	if err != nil {
-		log.Fatal(err)
+	switch os.Args[1] {
+	case "filetest":
+		filetest()
+	case "httpget":
+		httpget()
+	default:
+		fmt.Println("unknown request")
 	}
-	nread := len(data)
-	fmt.Println("Bytes Read: ", nread)
-	fmt.Printf("%v", data)
-	fmt.Println()
-	fmt.Printf("%s", data)
-
-	var sr []Record
-
-	err = json.Unmarshal(data, &sr)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("sr")
-	fmt.Print(sr)
-	fmt.Println()
-
-	out, err := json.Marshal(sr)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Printf("%s", out)
-
 }
